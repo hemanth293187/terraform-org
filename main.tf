cc618b5a29b232a421cdeba07c380cc50b3cbd6c
hemanths <hemanthd.work@gmail.com>
2025-04-21 12:41:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-04985531f48a27ae7"
 instance_type = "t2.medium"
-key_name = "argocd"
+key_name = "HCP"
 vpc_security_group_ids = ["sg-0cd64c9f4dd692ecf"]
 tags = {
 Name = var.instance_names[count.index]
