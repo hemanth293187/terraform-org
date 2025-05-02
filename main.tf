036f4f3620a158236b8df3f864cb8b0add3f78b4
hemanths <hemanthd.work@gmail.com>
2025-05-02 23:13:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,5 +14,5 @@ Name = var.instance_names[count.index]
 }
 
 variable "instance_names" {
-default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
+default = ["jenkins", "tomcat-1", "tomcat-2", "nexus"]
 }
